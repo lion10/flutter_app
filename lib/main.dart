7a0db7c5dd3eea00cb0f6de756ab934f7063fc7c
omar suliman abu sabha <ssomar2028@gmail.com>
2020-01-20 10:58:52 +0200

    add provider plugines

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_firebase/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'models/user.dart';
 import 'screens/wrapper.dart';
 
 void main() => runApp(MyApp());
@@ -6,9 +9,14 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
+
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    // I make stream provider type user cuz value return Stream<User> from AuthService yea
+    return StreamProvider<User>.value(
+        value: AuthService().user,
+        child :MaterialApp(
+         home: Wrapper(),
+      )
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_firebase/models/user.dart';
 import 'package:flutter_app_firebase/screens/authenticate/authenticate.dart';
+import 'package:provider/provider.dart';
 import 'home/home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    // I make stream provider type user cuz value return StreamProvider<User> from main yea
+
+    final user =Provider.of<User>(context);
+    print(user);
     // return either home or authenticate widget
     return  Authenticate();
   }
