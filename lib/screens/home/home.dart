38d7ab515d63fb3f4480c07d00da936f468bdfbc
omar suliman abu sabha <ssomar2028@gmail.com>
2020-01-22 00:05:31 +0200

    add modal bottom sheet in home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -13,6 +13,19 @@ class Home extends StatelessWidget {
   @override
 
   Widget build(BuildContext context) {
+
+    // i make this func here in build cuz i need context yea ...
+    void _showSettingPanel(){
+      showModalBottomSheet(context: context,
+          builder: (context){
+              return Container(
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+                child: Text('bottomSheet'),
+              ) ;
+          });
+    }
+
+
     return StreamProvider<List<Brew>>.value(
      value: DatabaseServices().brews, // here brews is func return stream yea
      child: Scaffold(
@@ -28,7 +41,12 @@ class Home extends StatelessWidget {
                   onPressed:() async {
                     await _auth.signOut();
                   },
-              )
+              ),
+          FlatButton.icon(
+            icon:Icon(Icons.settings),
+            label: Text('Setting'),
+            onPressed:() => _showSettingPanel(),
+          ),
            ],
         ),
        body: BrewList(),
